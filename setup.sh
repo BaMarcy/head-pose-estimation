05cf623842675d7c7d2bddb0e6ed175a89d779df
unknown <balogh.marcell.dk@gmail.com>
2022-06-08 16:31:06 +0200

    some message

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "
-[general]n
-email = "anylingual.ai@gmail.com"n
-" > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
+
+echo "[server]
+headless = true
+enableCORS=false
+port = $PORT
 " > ~/.streamlit/config.toml
